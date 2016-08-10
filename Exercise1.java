d5e7f4891cd4035d0a15bef438b6bc8610179cd3
olliee <oliverhandford7@gmail.com>
2016-08-10 15:37:45 +0100

    Question 1

---
+++ Exercise1.java
@@ -0,0 +1,9 @@
+public class Exercise1
+{
+   public static void main (String[] args)
+   {
+       double km = 30, miles;
+       miles = km * 5 / 8;
+       System.out.println(km + " kilometres is " + miles + " miles");
+   }//end main
+}//end class
